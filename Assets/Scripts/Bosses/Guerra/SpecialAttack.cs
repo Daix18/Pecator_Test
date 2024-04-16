fe358db1b9f3783913c67a4104c5a8c96b18167e
Daniel Izaguirre Montalvo <danizaguirre10@gmail.com>
2024-04-16 15:09:21 +0200

    Adjustments

--- Assets/Scripts/Bosses/Guerra/SpecialAttack.cs
+++ Assets/Scripts/Bosses/Guerra/SpecialAttack.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class SpecialAttack : MonoBehaviour
@@ -7,10 +8,15 @@ public class SpecialAttack : MonoBehaviour
     public static SpecialAttack THIS;
     [Header("Settings")]
     [SerializeField] private GameObject area;
+    [SerializeField] private Transform fatherArrowRain;
     [SerializeField] private float damage;
     [SerializeField] private float duration;
     private ParticleSystem sistemaDeParticulas;
     private new BoxCollider2D collider2D;
+
+    // Limites para el movimiento en el eje X
+    [SerializeField] private float minXLimit = -5f;
+    [SerializeField] private float maxXLimit = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -41,6 +47,9 @@ public class SpecialAttack : MonoBehaviour
     {
         collider2D.enabled = true;
         sistemaDeParticulas.Play();
+        Vector2 originalFatherPosition = fatherArrowRain.transform.position;
+        float randomXPosition = Random.Range(minXLimit, maxXLimit);
+        //Vector2 newPosition = (randomXPosition,  originalFatherPosition.y);
         yield return new WaitForSeconds(duration);
         collider2D.enabled = false;
         sistemaDeParticulas.Stop();
