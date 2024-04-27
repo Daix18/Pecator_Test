4b1b1f9b40634f43d90f56763fc2d54753c1fe68
WookiePookieBear <solefran7@gmail.com>
2024-04-27 15:10:34 +0200

    faltan cosas  pero continuo despues

---
+++ Assets/ButtonInfo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonInfo : MonoBehaviour
+{
+    public int ItemID;
+    public Text PriceTxt;
+    public Text QuantityTxt;
+    public GameObject ShopManager;
+    void Update()
+    {
+        
+    }
+}
