8a9acfc1e87d549c38e4b41bfa6b37c388732751
WookiePookieBear <solefran7@gmail.com>
2024-04-28 19:14:55 +0200

    para tu

--- Assets/ShopManagerScript.cs
+++ Assets/ShopManagerScript.cs
@@ -7,12 +7,17 @@ using UnityEngine.EventSystems;
 
 public class ShopManagerScript : MonoBehaviour
 {
-    public int[,] shopItems = new int[5,5];
+    public int[,] shopItems = new int[5, 5];
     public int cantidadMonedas;
     public TextMeshProUGUI CoinsTXT;
+    public GameObject shopUI;
+
+    // Variable para controlar si los ítems con ID 1 y 2 han sido comprados
+    private bool[] itemPurchased = new bool[5];
+
     void Start()
     {
-        CoinsTXT.text =  "Monedas:" + cantidadMonedas.ToString();
+        CoinsTXT.text = "Monedas:" + cantidadMonedas.ToString();
 
         //ID's
         shopItems[1, 1] = 1;
@@ -26,7 +31,7 @@ public class ShopManagerScript : MonoBehaviour
         shopItems[2, 3] = 25;
         shopItems[2, 4] = 30;
 
-        //Quantity
+        //Cantidad
         shopItems[3, 1] = 0;
         shopItems[3, 2] = 0;
         shopItems[3, 3] = 0;
@@ -36,17 +41,30 @@ public class ShopManagerScript : MonoBehaviour
     public void Buy()
     {
         GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
-        if (cantidadMonedas >= shopItems[2, ButtonRef.GetComponent<ButtonInfo>().ItemID])
+        int itemID = ButtonRef.GetComponent<ButtonInfo>().ItemID;
+        int itemPrice = shopItems[2, itemID];
+
+        // Verificar si hay suficientes monedas y si el ítem puede comprarse
+        if (cantidadMonedas >= itemPrice && (itemID == 3 || itemID == 4 || !itemPurchased[itemID]))
         {
-            cantidadMonedas -= shopItems[2, ButtonRef.GetComponent<ButtonInfo>().ItemID];
-            shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID]++;
-            ButtonRef.GetComponent<ButtonInfo>().QuantityTxt.text = shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID].ToString() ;
+            cantidadMonedas -= itemPrice;
+            shopItems[3, itemID]++;
+            ButtonRef.GetComponent<ButtonInfo>().QuantityTxt.text = shopItems[3, itemID].ToString();
+            UpdateCoinsText(); // Actualizar el texto de las monedas después de comprar
+
+            // Marcar el ítem como comprado si tiene ID 1 o 2
+            if (itemID == 1 || itemID == 2)
+                itemPurchased[itemID] = true;
         }
     }
-    void Update()
-    {
-        cantidadMonedas  =  GameController.THIS.cantidadMonedas;
-        CoinsTXT.text = "Monedas: " + cantidadMonedas;
 
+    void UpdateCoinsText()
+    {
+        CoinsTXT.text = "Monedas: " + cantidadMonedas;
+    }
+    private void Update()
+    {
+        cantidadMonedas = GameController.THIS.cantidadMonedas;
+        CoinsTXT.text = "Monedas: " + cantidadMonedas;
     }
 }
